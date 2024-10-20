46078359595b9cc4e041caa961091b390519655e
Ops Eng <opsengmb@gmail.com>
2024-10-20 22:46:39 +0800

    single file for bridge

--- terraform/alicloud/bridge.tf
+++ terraform/alicloud/bridge.tf
@@ -1,7 +1,54 @@
-provider "alicloud" {
-    alias  = "bridge"
-    region = var.bridge_region
+module "bridge_vpc" {
+  provider          = alicloud.bridge
+  source  = "alibaba/vpc/alicloud"
+
+  create            = true
+  vpc_name          = "${var.env_name}-${var.project}-vpc"
+  vpc_cidr          = var.bridge_vpc_cidr
+  resource_group_id = alicloud_resource_manager_resource_group.rg.id
+
+  availability_zones = [var.bridge_az_a]
+  vswitch_cidrs      = [var.bridge_pub_a]
+
+  vpc_tags = {
+    Environment = var.env_name
+    Name        = "${var.env_name}-${var.project}-vpc"
+  }
+
+  vswitch_tags = {
+    Environment  = var.env_name
+  }
 }
+
+resource "alicloud_nat_gateway" "bridge_int_nat_gw1" {
+  provider          = alicloud.bridge
+  vpc_id           = module.bridge_vpc.vpc_id
+  nat_gateway_name = "${var.env_name}-${var.project}-ingw1"
+  payment_type     = "PayAsYouGo"
+  vswitch_id       = module.bridge_vpc.vswitch_ids[0]
+  nat_type         = "Enhanced"
+}
+
+resource "alicloud_eip_association" "bridge_int_nat_assoc1" {
+  provider          = alicloud.bridge
+  allocation_id = alicloud_eip_address.bridge_eip_addr_snat1.id
+  instance_type = "Nat"
+  instance_id   = alicloud_nat_gateway.bridge_int_nat_gw1.id
+}
+
+resource "alicloud_eip_address" "bridge_eip_addr_snat1" {
+  provider          = alicloud.bridge
+  address_name  = "${var.env_name}-${var.project}-eipaddr1"
+  resource_group_id = alicloud_resource_manager_resource_group.rg.id 
+}
+
+resource "alicloud_snat_entry" "bridge_int_nat_snat1" {
+  provider          = alicloud.bridge
+  snat_table_id     = alicloud_nat_gateway.bridge_int_nat_gw1.snat_table_ids
+  source_vswitch_id = module.vpc.vswitch_ids[0]
+  snat_ip           = alicloud_eip_address.bridge_eip_addr_snat1.ip_address
+}
+
 
 resource "alicloud_security_group" "bridge-sg" {
   provider          = alicloud.bridge
