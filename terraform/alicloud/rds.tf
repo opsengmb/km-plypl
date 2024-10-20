aa76204598679de69da17f4d487f72413c5fa1d0
Ops Eng <opsengmb@gmail.com>
2024-10-20 20:47:22 +0800

    added vars

--- terraform/alicloud/rds.tf
+++ terraform/alicloud/rds.tf
@@ -21,11 +21,9 @@ resource "alicloud_db_instance" "default" {
   instance_type            = var.db_instance_type
   instance_storage         = var.db_instance_storage
   instance_name            = "${var.env_name}-${var.project}-master-rds"
-  zone_id                  = data.alicloud_db_zones.default.zones.0.id
   vswitch_id               = module.vpc.vswitch_ids[0]
   monitoring_period        = "60"
   db_instance_storage_type = "cloud_essd"
-  security_ips             = [var.vpc_cidr]
   category                 = var.db_category
   security_group_ids       = [alicloud_security_group.rds-sg.id]
 }
