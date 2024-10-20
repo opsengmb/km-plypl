c47772f7dac9c9980786dbc6c598cb8e3454304f
Ops Eng <opsengmb@gmail.com>
2024-10-20 10:17:40 +0800

    update provider

--- terraform/alicloud/main.tf
+++ terraform/alicloud/main.tf
@@ -4,7 +4,7 @@ terraform {
     required_version = ">=1.0"
     required_providers {
         alibabacloudstack = {
-        source  = "registry.terraform.io/terraform-alibabacloudstack/alibabacloudstack"
+        source  = "registry.terraform.io/aliyun/alicloud"
         version = ">= 1.0.0"
      }
   }
