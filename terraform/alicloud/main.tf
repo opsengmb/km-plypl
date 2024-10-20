b1a39959b4d49124f555f4cb8d35fd2515e6c3ab
Ops Eng <opsengmb@gmail.com>
2024-10-20 10:04:41 +0800

    add new rtb

--- terraform/alicloud/main.tf
+++ terraform/alicloud/main.tf
@@ -1,11 +1,15 @@
-// alibaba cloud provider with region
-provider "alicloud" {
-    region = var.region
-    // latest alicloud provider version
-    version = "1.133.0"
-}
 
 // terraform provider with latest required_version
 terraform {
     required_version = ">=1.0"
+}
+
+terraform {
+  required_providers {
+    alibabacloudstack = {
+      source  = "registry.terraform.io/terraform-alibabacloudstack/alibabacloudstack"
+      version = ">= 1.0.0"
+      region = var.region
+    }
+  }
 }
