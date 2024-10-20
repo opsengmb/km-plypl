b47fd08a5c4a739af8f43525d49dec90c2e8af05
Ops Eng <opsengmb@gmail.com>
2024-10-20 11:08:45 +0800

    update sgrp

--- terraform/alicloud/ecs.tf
+++ terraform/alicloud/ecs.tf
@@ -1,5 +1,5 @@
 resource "alicloud_security_group" "be-sg" {
-  name        = "${var.env_name}-${var.project}-fe-sg"
+  name        = "${var.env_name}-${var.project}-be-sg"
   description = "${var.env_name}-${var.project} security group"
   vpc_id = module.vpc.vpc_id
 }
