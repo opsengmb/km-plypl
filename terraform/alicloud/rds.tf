870bcd9a188727410f6a6f5b204878c97373297e
Ops Eng <opsengmb@gmail.com>
2024-10-20 18:31:48 +0800

    add bridge instance and update LB zones

--- terraform/alicloud/rds.tf
+++ terraform/alicloud/rds.tf
@@ -1,10 +1,8 @@
 data "alicloud_db_zones" "default" {}
 
 resource "alicloud_db_instance" "default" {
-  #engine                   = "MariaDB"
-  #engine_version           = "10.3"
-  engine                   = "MySQL"
-  engine_version           = "8.0"
+  engine                   = "MariaDB"
+  engine_version           = "10.3"
   instance_type            = "mysql.x4.large.2c"
   instance_storage         = "200"
   instance_name            = "${var.env_name}-${var.project}-master-rds"
