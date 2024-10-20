536fbe7da5c3b0d110ae0e9bb59597b0dd16637b
Ops Eng <opsengmb@gmail.com>
2024-10-20 10:06:29 +0800

    add provider

--- terraform/alicloud/main.tf
+++ terraform/alicloud/main.tf
@@ -2,14 +2,15 @@
 // terraform provider with latest required_version
 terraform {
     required_version = ">=1.0"
+    required_providers {
+        alibabacloudstack = {
+        source  = "registry.terraform.io/terraform-alibabacloudstack/alibabacloudstack"
+        version = ">= 1.0.0"
+     }
+  }
 }
 
-terraform {
-  required_providers {
-    alibabacloudstack = {
-      source  = "registry.terraform.io/terraform-alibabacloudstack/alibabacloudstack"
-      version = ">= 1.0.0"
-      region = var.region
-    }
-  }
+// add alicloud provider
+provider "alicloud" {
+  region = var.region
 }
