711d9a0e6bbc411b1b56229c858a606c3f684474
Ops Eng <peraltadavidpaolo@gmail.com>
2024-12-08 02:18:41 +0800

    deploy dev

--- terraform/alicloud/ecs-be.tf
+++ terraform/alicloud/ecs-be.tf
@@ -1,4 +1,5 @@
 resource "alicloud_security_group" "be-sg" {
+  count = var.env_name != "dev" ? 1 : 0
   resource_group_id = alicloud_resource_manager_resource_group.rg.id
   name        = "${var.env_name}-${var.project}-be-sg"
   description = "${var.env_name}-${var.project} security group"
@@ -6,50 +7,56 @@ resource "alicloud_security_group" "be-sg" {
 }
 
 resource "alicloud_security_group_rule" "be-https" {
+  count = var.env_name != "dev" ? 1 : 0
   type              = "ingress"
   ip_protocol       = "tcp"
   port_range        = "80/80"
-  security_group_id = alicloud_security_group.be-sg.id
+  security_group_id = alicloud_security_group.be-sg[count.index].id
   cidr_ip           = var.vpc_cidr
 }
 
 resource "alicloud_security_group_rule" "be-http-egress" {
+  count = var.env_name != "dev" ? 1 : 0
   type              = "egress"
   ip_protocol       = "tcp"
   port_range        = "80/80"
-  security_group_id = alicloud_security_group.be-sg.id
+  security_group_id = alicloud_security_group.be-sg[count.index].id
   cidr_ip           = "0.0.0.0/0"
 }
 
 resource "alicloud_security_group_rule" "be-db-egress" {
+  count = var.env_name != "dev" ? 1 : 0
   type              = "egress"
   ip_protocol       = "tcp"
   port_range        = "3306/3306"
-  security_group_id = alicloud_security_group.be-sg.id
+  security_group_id = alicloud_security_group.be-sg[count.index].id
   cidr_ip           = "0.0.0.0/0"
 }
 
 resource "alicloud_security_group_rule" "be-https-egress" {
+  count = var.env_name != "dev" ? 1 : 0
   type              = "egress"
   ip_protocol       = "tcp"
   port_range        = "443/443"
-  security_group_id = alicloud_security_group.be-sg.id
+  security_group_id = alicloud_security_group.be-sg[count.index].id
   cidr_ip           = "0.0.0.0/0"
 }
 
 resource "alicloud_security_group_rule" "be-udp-dns-egress" {
+  count = var.env_name != "dev" ? 1 : 0
   type              = "egress"
   ip_protocol       = "udp"
   port_range        = "53/53"
-  security_group_id = alicloud_security_group.be-sg.id
+  security_group_id = alicloud_security_group.be-sg[count.index].id
   cidr_ip           = "0.0.0.0/0"
 }
 
 resource "alicloud_security_group_rule" "be-tcp-dns-egress" {
+  count = var.env_name != "dev" ? 1 : 0
   type              = "egress"
   ip_protocol       = "tcp"
   port_range        = "53/53"
-  security_group_id = alicloud_security_group.be-sg.id
+  security_group_id = alicloud_security_group.be-sg[count.index].id
   cidr_ip           = "0.0.0.0/0"
 }
 
@@ -60,7 +67,7 @@ resource "alicloud_instance" "gl_be_ecs_instance_1" {
   instance_name        = "${var.env_name}-${var.project}-bo-be"
   image_id             = var.bo_be_image_id
   instance_type        = "ecs.g7.large"
-  security_groups      = [alicloud_security_group.be-sg.id]
+  security_groups      = [alicloud_security_group.be-sg[count.index].id]
   vswitch_id           = module.vpc.vswitch_ids[1]
   password             = "dynamic_random_password"
   system_disk_category = "cloud_essd"
@@ -73,11 +80,12 @@ resource "alicloud_instance" "gl_be_ecs_instance_1" {
 
 // THIS IS GL BE
 resource "alicloud_instance" "bo_be_ecs_instance_1" {
+  count = var.env_name != "dev" ? 1 : 0
   resource_group_id    = alicloud_resource_manager_resource_group.rg.id 
   instance_name        = "${var.env_name}-${var.project}-gl-be"
   image_id             = var.gl_be_image_id
   instance_type        = "ecs.g7.large"
-  security_groups      = [alicloud_security_group.be-sg.id]
+  security_groups      = [alicloud_security_group.be-sg[count.index].id]
   vswitch_id           = module.vpc.vswitch_ids[1]
   password             = "dynamic_random_password"
   system_disk_category = "cloud_essd"
